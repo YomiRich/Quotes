0d55a74a489b943384659f92779e94f6644a0430
Yomi Rich <ymueni@gmail.com>
2019-07-08 09:27:43 +0300

    Add quotes and authors successfully

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,9 +10,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    {id:1, author:'― Martin Fowler',quote:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.'},
-    {id:2, author:'― Pastor Chris Oyakhilome',quote:'When you got born again your past was not erased, it became non-existent.'},
-    {id:3, author:'― Kent Beck',quote:'I am not a great programmer; I am just a good programmer with great habits.'},
+    {id:1, quote:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',author:'― Martin Fowler',},
+    {id:2, quote:'When you got born again your past was not erased, it became non-existent.',author:'― Pastor Chris Oyakhilome',},
+    {id:3,quote:'I am not a great programmer; I am just a good programmer with great habits.', author:'― Kent Beck',},
       
   ];
   constructor() { }
