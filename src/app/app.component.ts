081f648c3dd70cb2528f6c6142a746a021c18230
Yomi Rich <ymueni@gmail.com>
2019-07-08 08:17:19 +0300

    Add class Quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+ 
+  quotes:string[];
+
+  constructor(){
+    this.quotes = ['Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'When you got born again your past was not erased, it became non-existent.', 'I am not a great programmer; I am just a good programmer with great habits.']
+  } 
 }
+
